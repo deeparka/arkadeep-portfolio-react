545cb2c734833aec7c4e54687f71d98340dcb4eb
deeparka <arkadeep180@gmail.com>
2022-09-02 01:08:30 +0530

    completed about section with styling

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import image from "../images/Arkadeep-top-right.png";
+import imageTwo from "../images/Screenshot from 2022-09-01 22-40-58.png";
 
 export const Home = () => {
     return (
@@ -15,6 +16,27 @@ export const Home = () => {
                 </div>
                 <div className="right-image">
                     <img src={image} alt="Arkadeep.png" />
+                </div>
+            </div>
+            <div className="about-img">
+                <div className="img-arka">
+                    <img src={imageTwo} alt="About.png" />
+                </div>
+                <div className="about">
+                    <div className="about-me">About me</div>
+                    <div className="about-me-description">
+                        <span>
+                            Hello there, I’m <span>Arkadeep Prasad</span>.
+                        </span>{" "}
+                        I’m a self-taught UI/UX & Front-End Designer from West
+                        Bengal, India. I follow my passion for designing & also
+                        find it very interesting & enjoyable. I am passionate
+                        about learning new technologies. I am always keen to
+                        work and experience things.
+                    </div>
+                    <button className="btn-resume" type="button">
+                        Resume
+                    </button>
                 </div>
             </div>
         </div>
