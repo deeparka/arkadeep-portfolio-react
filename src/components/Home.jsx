5c40e8967792034fffbbd04d44dfccf3a03d8173
deeparka <arkadeep180@gmail.com>
2022-12-07 01:05:07 +0530

    NavBar completed

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Home = () => {
   return (
-    <div className='text-3xl underline font-black'>Home</div>
+    <div className="">Home</div>
   )
 }
 
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -5,9 +5,9 @@ const NavBar = () => {
         <nav className="flex py-5 justify-between">
             <h1 className="font-bold text-lg">Arkadeep Prasad</h1>
             <div className="flex">
-                <div className="font-bold mr-10 cursor-pointer" onClick={()=> console.log('You clicked about')}>About</div>
-                <div className="font-bold mr-10 cursor-pointer" onClick={()=> console.log('You clicked skills')}>Skills</div>
-                <div className="font-bold mr-10 cursor-pointer" onClick={()=> console.log('You clicked project')}>Project</div>
+                <div className="font-bold mr-16 cursor-pointer" onClick={()=> console.log('You clicked about')}>About</div>
+                <div className="font-bold mr-16 cursor-pointer" onClick={()=> console.log('You clicked skills')}>Skills</div>
+                <div className="font-bold mr-16 cursor-pointer" onClick={()=> console.log('You clicked project')}>Project</div>
                 <div className="font-bold cursor-pointer" onClick={()=> console.log('You clicked contact')}>Contact</div>
             </div>
         </nav>
