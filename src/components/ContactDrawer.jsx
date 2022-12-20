5425a558c5366c468d4f07cf47a7bfbd974252c7
deeparka <arkadeep180@gmail.com>
2022-12-20 18:36:29 +0530

    built ContactDrawer component

---
+++ src/components/ContactDrawer.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function ContactDrawer() {
+  return (
+    <div className='fixed w-auto h-24 top-1/2 bg-customBlack'>ContactDrawer</div>
+  )
+}
+
+export default ContactDrawer
