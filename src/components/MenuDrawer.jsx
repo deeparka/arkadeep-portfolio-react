077ff0f1229dddb1686de7c3c080afef28402680
deeparka <arkadeep180@gmail.com>
2022-12-22 01:15:45 +0530

    initial commit

--- src/components/MenuDrawer.jsx
+++ src/components/MenuDrawer.jsx
@@ -1,6 +1,10 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import { handleAboutClick, handleContactClick, handleSkillsClick } from "../lib/helper";
+import {
+  handleAboutClick,
+  handleContactClick,
+  handleSkillsClick,
+} from "../lib/helper";
 
 function MenuDrawer() {
   return (
@@ -8,19 +12,25 @@ function MenuDrawer() {
       id="drawer-example"
       className="fixed top-68px left-0 z-40 h-auto px-11 py-6 overflow-y-auto bg-customGrey w-full text-customWhite border-t-customBlack border-t-2"
     >
-      <Link to='/'>
+      <Link to="/">
         <p className="text-xl font-semibold py-2">Home</p>
       </Link>
       <hr className="mb-1" />
-      <p className="text-xl font-semibold py-2" onClick={handleAboutClick}>About</p>
+      <p className="text-xl font-semibold py-2" onClick={handleAboutClick}>
+        About
+      </p>
       <hr className="mb-1" />
-      <p className="text-xl font-semibold py-2" onClick={handleSkillsClick}>Skills</p>
+      <p className="text-xl font-semibold py-2" onClick={handleSkillsClick}>
+        Skills
+      </p>
       <hr className="mb-1" />
-      <Link to='/projects'>
+      <Link to="/projects">
         <p className="text-xl font-semibold py-2">Project</p>
       </Link>
       <hr className="mb-1" />
-      <p className="text-xl font-semibold py-2" onClick={handleContactClick}>Contact</p>
+      <p className="text-xl font-semibold py-2" onClick={handleContactClick}>
+        Contact
+      </p>
     </div>
   );
 }
